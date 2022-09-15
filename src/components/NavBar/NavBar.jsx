33a73eaad02572f277edcd9435f54de9bd633916
Federico Anelli <federicoanelli@gmail.com>
2022-09-14 21:24:04 -0300

    Navbar top

---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,21 @@
+import "./NavBar.css";
+
+const NavBar = () => {
+
+    return (
+        <nav className="navbar">
+            <div className="topNav">
+                <ul className="topNavUl">
+                    <li className="topNavTexts"><strong>Commercial:</strong> 954-436-8899 | <strong>Residential:</strong> 954-901-9025</li>
+                    <li className="topNavTexts">actionpainting1@att.net</li>
+                    <li className="topNavTexts"><strong>5200 SW 164th Terrace South West Ranches, FL 33331</strong></li>
+                </ul>
+            </div>
+            <div className="bottomNav">
+
+            </div>
+        </nav>
+    );
+    }
+
+export default NavBar;
