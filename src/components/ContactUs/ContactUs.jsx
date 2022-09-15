e65c556768fcd2092070df05813fcc8c2320e217
Federico Anelli <federicoanelli@gmail.com>
2022-09-15 10:06:52 -0300

    Working contact us form

--- src/components/ContactUs/ContactUs.jsx
+++ src/components/ContactUs/ContactUs.jsx
@@ -1,6 +1,33 @@
 import "./ContactUs.css";
+import { useRef } from "react";
+import emailjs from '@emailjs/browser';
+import Swal from "sweetalert2";
+
 
 const ContactUs = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        emailjs.sendForm('service_gkq2fd5', 'template_23zs7jg', form.current, 'qXV_XN7IgM6S6ww9N')
+            .then((result) => {
+                Swal.fire({
+                    position: 'top-end',
+                    icon: 'success',
+                    title: 'Your message has been sent.',
+                    showConfirmButton: false,
+                    timer: 1500
+                  })
+                e.target.reset();
+            }, (error) => {
+                Swal.fire({
+                    icon: 'error',
+                    title: 'Oops...',
+                    text: 'Something went wrong! Error:' + error,
+                  })
+            });
+    };
 
     return (
         <div className="contactUs">
@@ -22,14 +49,14 @@ const ContactUs = () => {
                     <div className="generalText"><div className="yellowCircleBack"><span className="material-symbols-outlined">location_on</span></div><p>5200 SW 164th Terrace South West Ranches, FL 33331</p></div>
                     <iframe className="googleMaps" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3584.3534045591787!2d-80.36983708499525!3d26.054704302769565!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x88d9a0d4912bf775%3A0x3159f2b4f5d563ee!2s5200%20SW%20164th%20Terrace%2C%20Southwest%20Ranches%2C%20FL%2033331%2C%20EE.%20UU.!5e0!3m2!1ses-419!2sar!4v1657113788527!5m2!1ses-419!2sar" allowFullScreen={true}></iframe>
                 </div>
-                <div className="contactForm">
-                    <input className="contactUsInput" type="text" placeholder="Full Name"/>
-                    <input className="contactUsInput" type="text" placeholder="Company"/>
-                    <input className="contactUsInput" type="tel" placeholder="Phone Number"/>
-                    <input className="contactUsInput" type="email" placeholder="Email"/>
-                    <textarea className="commentsArea" name="comments" id="comments" cols="30" rows="10" placeholder="Insert comment here"></textarea>
-                    <button className="contactUsButton">SEND</button>
-                </div>
+                <form className="contactForm" ref={form} onSubmit={sendEmail}>
+                    <input className="contactUsInput" type="text" name="from_name" placeholder="Full Name"/>
+                    <input className="contactUsInput" type="text" name="company_name" placeholder="Company"/>
+                    <input className="contactUsInput" type="tel"  name="phone_number" placeholder="Phone Number"/>
+                    <input className="contactUsInput" type="email" name="from_email" placeholder="Email"/>
+                    <textarea className="commentsArea" name="message" id="comments" cols="30" rows="10" placeholder="Insert comment here"></textarea>
+                    <button className="contactUsButton" type="submit" value="Send">SEND</button>
+                </form>
             </div>
 
         </div>
